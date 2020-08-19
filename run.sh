ed07b87badea64ceac1597b27aa23a57892fafd7
rex <yuxinwen>
2020-08-19 16:12:13 +0800

    fea: add Xiang's attack script

--- run.sh
+++ run.sh
@@ -5,14 +5,21 @@ python Provider/gen_data_mat.py --out_datadir ./Data -outc 10 -outn 25 --npoint 
 # if want dense npoints
 #python Provider/gen_data_mat.py --out_datadir ./Data -outc 10 -outn 25 --npoint 1024 --is_using_virscan --dense_npoints 10000
 
-# GeoA3 attack
+## GeoA3 attack
 python main_attack.py --data_dir_file Data/modelnet10_250instances1024_PointNet.mat --npoint 1024 \
     --arch PointNet --attack GeoA3 --attack_label All \
     --binary_max_steps 10 --iter_max_steps 500 --lr 0.01 \
-	--cls_loss_type CE --dis_loss_type CD --dis_loss_weight 1.0 \
+	--cls_loss_type CE \
+    --dis_loss_type CD --dis_loss_weight 1.0 \
     --hd_loss_weight 0.1 \
     --curv_loss_weight 1.0 --curv_loss_knn 16
-# Robust Attack
+## Xiang's attack
+python main_attack.py --data_dir_file Data/modelnet10_250instances1024_PointNet.mat --npoint 1024 \
+    --arch PointNet --attack Xiang --attack_label All \
+    --binary_max_steps 10 --iter_max_steps 500 --lr 0.01 \
+    --cls_loss_type Margin --confidence 0 \
+    --dis_loss_weight 1.0
+## Robust Attack
 python main_attack.py --data_dir_file Data/modelnet10_250instances1024_PointNet.mat --npoint 1024 \
     --dense_data_dir_file Data/modelnet10_250instances10000_PointNet.mat --is_save_normal \
     --arch PointNet --attack RA --attack_label All \
@@ -22,6 +29,7 @@ python main_attack.py --data_dir_file Data/modelnet10_250instances1024_PointNet.
     --knn_smoothing_loss_weight 5.0 --knn_smoothing_k 5 --knn_threshold_coef 1.1 \
     --cc_linf 0.1
 
+## Defense
 python defense.py --datadir Exps/PointNet_npoint1024/All/Pertub_0_BiStep10_IterStep500_Optadam_Lr0.01_Initcons10_CE_CDLoss1.0_HDLoss0.1_CurLoss1.0_k16/Mat \
 	--npoint 1024 --arch PointNet \
 	--defense_type outliers_fixNum --drop_num 128
@@ -38,14 +46,21 @@ python Provider/gen_data_mat.py --out_datadir ./Data -outc 10 -outn 25 --npoint 
 # if want dense npoints
 #python Provider/gen_data_mat.py --out_datadir ./Data -outc 10 -outn 25 --npoint 1024 --is_using_virscan --dense_npoints 10000 --arch PointNetPP
 
-# GeoA3 attack
+## GeoA3 attack
 python main_attack.py --data_dir_file Data/modelnet10_250instances1024_PointNetPP.mat --npoint 1024 \
     --arch PointNetPP --attack GeoA3 --attack_label All \
     --binary_max_steps 1 --iter_max_steps 500 --lr 1e-3 \
-	--cls_loss_type CE --dis_loss_type CD --dis_loss_weight 1.0 \
+	--cls_loss_type CE \
+    --dis_loss_type CD --dis_loss_weight 1.0 \
     --hd_loss_weight 0.1 \
     --curv_loss_weight 1.0 --curv_loss_knn 16
-# Robust Attack
+## Xiang's attack
+python main_attack.py --data_dir_file Data/modelnet10_250instances1024_PointNetPP.mat --npoint 1024 \
+    --arch PointNetPP --attack Xiang --attack_label All \
+    --binary_max_steps 10 --iter_max_steps 500 --lr 1e-3 \
+    --cls_loss_type Margin --confidence 0 \
+    --dis_loss_weight 1.0
+## Robust Attack
 python main_attack.py --data_dir_file Data/modelnet10_250instances1024_PointNetPP.mat --npoint 1024 \
     --dense_data_dir_file Data/modelnet10_250instances10000_PointNetPP.mat --is_save_normal \
     --arch PointNetPP --attack RA --attack_label All \
@@ -55,21 +70,29 @@ python main_attack.py --data_dir_file Data/modelnet10_250instances1024_PointNetP
     --knn_smoothing_loss_weight 5.0 --knn_smoothing_k 5 --knn_threshold_coef 1.1 \
     --cc_linf 0.1
 
+## Defense
 python defense.py --datadir Exps/PointNetPP_npoint1024/All/Pertub_0_BiStep10_IterStep500_Optadam_Lr0.01_Initcons10_CE_CDLoss1.0_HDLoss0.1_CurLoss1.0_k16/Mat \
 	--npoint 1024 --arch PointNetPP \
 	--defense_type outliers_fixNum --drop_num 128
 
+
+
 # For DGCNN
-
-
-# GeoA3 attack
+## GeoA3 attack
 python main_attack.py --data_dir_file Data/modelnet10_250instances1024_DGCNN.mat --npoint 1024 \
     --arch DGCNN --attack GeoA3 --attack_label All \
     --binary_max_steps 10 --iter_max_steps 500 --lr 1e-3 \
-	--cls_loss_type CE --dis_loss_type CD --dis_loss_weight 1.0 \
+	--cls_loss_type CE \
+    --dis_loss_type CD --dis_loss_weight 1.0 \
     --hd_loss_weight 0.1 \
     --curv_loss_weight 1.0 --curv_loss_knn 16
-# Robust Attack
+## Xiang's attack
+python main_attack.py --data_dir_file Data/modelnet10_250instances1024_DGCNN.mat --npoint 1024 \
+    --arch DGCNN --attack Xiang --attack_label All \
+    --binary_max_steps 10 --iter_max_steps 500 --lr 1e-3 \
+    --cls_loss_type Margin --confidence 0 \
+    --dis_loss_weight 1.0
+## Robust Attack
 python main_attack.py --data_dir_file Data/modelnet10_250instances1024_DGCNN.mat --npoint 1024 \
     --dense_data_dir_file Data/modelnet10_250instances10000_DGCNN.mat --is_save_normal \
     --arch DGCNN --attack RA --attack_label All \
