bb4c01436b8c9de52dede07c262fb87e51a732f0
rex <yuxinwen>
2020-10-05 17:36:54 +0800

    opt: can save mat now

--- Provider/save_ori_obj.py
+++ Provider/save_ori_obj.py
@@ -22,10 +22,8 @@ from torch.autograd.gradcheck import zero_gradients
 from pytorch3d.structures import Meshes
 from pytorch3d.io import load_obj, save_obj
 
-#from modelnet40_with_vert import ModelNet40_vert
-from modelnet_trn_test import ModelNetDataset
-
 parser = argparse.ArgumentParser(description='Saving ori obj mesh')
+parser.add_argument('--is_save_from_mat', action='store_true', default=False, help='')
 cfg  = parser.parse_args()
 print(cfg)
 
@@ -64,25 +62,45 @@ def pc_normalize_torch(point):
 
 
 def main():
-    test_dataset = ModelNetDataset(root='/data/modelnet40_normal_resampled/', batch_size=1, npoints=cfg.npoint, split='test', normal_channel=True)
-    #test_dataset = ModelNet40_vert('/data/ModelNet40/', 40, phase='test', regenerate_dataset=False)
-    test_loader = torch.utils.data.DataLoader(test_dataset, 1, shuffle=True, drop_last=True, num_workers=8, pin_memory=True)
-    test_size = test_dataset.__len__()
+    if cfg.is_save_from_mat:
+        mat_path = os.path.join('../Data', 'modelnet40_2111instances10000_PointNet.mat')
+        dataset = sio.loadmat(mat_path)
+        trg_pc = torch.FloatTensor(dataset["data"]).cuda()
 
-    if not os.path.exists(os.path.join('../Data', 'Ten_class_ori_mesh')):
-            os.makedirs(os.path.join('../Data', 'Ten_class_ori_mesh'))
+        if not os.path.exists(os.path.join('../Data', 'All_class_ori_mesh')):
+                os.makedirs(os.path.join('../Data', 'All_class_ori_mesh'))
 
-    for i, (vert, faces, label) in enumerate(test_loader):
-        if convert_from_modelnet40_1024_processed[label[0]] in label_indexes:
-            vert = vert.squeeze(0)
-            faces = faces.squeeze(0)
-            vert, _, _ = pc_normalize_torch(vert)
-            trg_mesh = Meshes(verts=[vert], faces=[faces]).cuda()
 
-            file_name = os.path.join('../Data', 'Ten_class_ori_mesh', str(i)+'_'+str(convert_from_modelnet40_1024_processed[label[0]])+'.obj')
-            final_verts, final_faces = trg_mesh.get_mesh_verts_faces(0)
-            print('Processing ['+str(i)+'/'+str(test_size)+' ] instance')
-            save_obj(file_name, final_verts, final_faces)
+        for i in range(trg_pc.size(0)):
+            curr_trg_pc = trg_pc[i].unsqueeze(0)
+            fout = open(os.path.join('../Data', 'All_class_ori_mesh', str(i)+'.xyz'), 'w')
+
+            for m in range(curr_trg_pc.shape[2]):
+                fout.write('%f %f %f \n' % (curr_trg_pc[0, 0, m], curr_trg_pc[0, 1, m], curr_trg_pc[0, 2, m]))
+            fout.close()
+    else:
+        #from modelnet40_with_vert import ModelNet40_vert
+        from modelnet_trn_test import ModelNetDataset
+
+        test_dataset = ModelNetDataset(root='/data/modelnet40_normal_resampled/', batch_size=1, npoints=cfg.npoint, split='test', normal_channel=True)
+        #test_dataset = ModelNet40_vert('/data/ModelNet40/', 40, phase='test', regenerate_dataset=False)
+        test_loader = torch.utils.data.DataLoader(test_dataset, 1, shuffle=True, drop_last=True, num_workers=8, pin_memory=True)
+        test_size = test_dataset.__len__()
+
+        if not os.path.exists(os.path.join('../Data', 'Ten_class_ori_mesh')):
+                os.makedirs(os.path.join('../Data', 'Ten_class_ori_mesh'))
+
+        for i, (vert, faces, label) in enumerate(test_loader):
+            if convert_from_modelnet40_1024_processed[label[0]] in label_indexes:
+                vert = vert.squeeze(0)
+                faces = faces.squeeze(0)
+                vert, _, _ = pc_normalize_torch(vert)
+                trg_mesh = Meshes(verts=[vert], faces=[faces]).cuda()
+
+                file_name = os.path.join('../Data', 'Ten_class_ori_mesh', str(i)+'_'+str(convert_from_modelnet40_1024_processed[label[0]])+'.obj')
+                final_verts, final_faces = trg_mesh.get_mesh_verts_faces(0)
+                print('Processing ['+str(i)+'/'+str(test_size)+' ] instance')
+                save_obj(file_name, final_verts, final_faces)
 
 
 if __name__ == '__main__':
